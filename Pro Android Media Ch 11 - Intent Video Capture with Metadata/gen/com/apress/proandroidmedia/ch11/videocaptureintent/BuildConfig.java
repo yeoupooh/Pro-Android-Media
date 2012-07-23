a0a6640e69369aaf4941c8e06f4f1233fef35e5d
Thomas Jinwoo Min <thomas.min.git@gmail.com>
2012-07-23 09:24:24 +0900

    Fix project setting.

---
+++ Pro Android Media Ch 11 - Intent Video Capture with Metadata/gen/com/apress/proandroidmedia/ch11/videocaptureintent/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.apress.proandroidmedia.ch11.videocaptureintent;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
